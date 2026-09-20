85d2a67f4b8537be90513b89492efad2e182d25a
一只炫彩玉米 <3783260249@qq.com>
2026-09-21 03:03:40 +0800

    Delete android directory

--- android/app/src/main/kotlin/fan/astral/next/game/WidgetPrefsExt.kt
+++
@@ -1,7 +0,0 @@
-package fan.astral.next.game
-
-import android.content.SharedPreferences
-
-/** Normalize SharedPreferences.getString nullability across API levels. */
-fun SharedPreferences.widgetString(key: String, default: String = ""): String =
-    getString(key, default) ?: default
