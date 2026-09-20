7d51882633e50b5212a58b8f82b1b389b19c0880
一只炫彩玉米 <3783260249@qq.com>
2026-09-21 03:05:57 +0800

    Delete tools directory

--- tools/valheim_astral_net/plugin/AstralValheim.cs
+++
@@ -1,9 +0,0 @@
-namespace AstralValheimNet
-{
-    internal static class AstralValheim
-    {
-        public const int GamePort = 2456;
-        public const int DiscoveryPort = 2460;
-        public const uint NetworkVersion = 36U;
-    }
-}
