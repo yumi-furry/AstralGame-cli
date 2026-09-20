8de9d5f883463806aad6b9f3f09a592cb09f3755
一只炫彩玉米 <3783260249@qq.com>
2026-09-21 03:06:18 +0800

    Delete windows directory

--- windows/runner/single_instance.h
+++
@@ -1,9 +0,0 @@
-#ifndef RUNNER_SINGLE_INSTANCE_H_
-#define RUNNER_SINGLE_INSTANCE_H_
-
-/// Returns true if this process should continue starting the app.
-/// If another instance is already running, returns false so the caller can
-/// forward `astralgame://` via app_links and exit.
-bool EnsureSingleInstance();
-
-#endif  // RUNNER_SINGLE_INSTANCE_H_
